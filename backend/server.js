d1b6ca7e51acbaa4183eba3081fa9eab3c1308fd
wrswk <147695187+wrswk@users.noreply.github.com>
2025-05-08 16:30:47 +0300

    db string add

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 //import pakage
 import express from "express";
+import mongoose from 'mongoose';
 import 'dotenv/config'
 
 const app = express()
 const port = process.env.PORT;
+const db_url= process.env.DBURL;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello Rashi!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, async() => {
+  await mongoose.connect(db_url);
+  console.log(`DB connected successfully!, Server is running on port no: ${port}`)
 })
